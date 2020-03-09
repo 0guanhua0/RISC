27cfc114a899f5f41e8b86c4a4207a69556e95ad
ananjaya.tyagi@duke.edu <ananjaya.tyagi@duke.edu>
2020-03-09 19:58:02 -0400

    Added game.java

--- server/src/main/java/edu/duke/ece651/risk/server/Game.java
+++ server/src/main/java/edu/duke/ece651/risk/server/Game.java
@@ -1,4 +1,30 @@
 package edu.duke.ece651.risk.server;
 
+import edu.duke.ece651.risk.shared.Server;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Game {
+    Server connect;
+    private Socket socket;
+    private String data;
+
+    public Game() throws IOException {
+        connect = new Server();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.acceptSocket();
+        game.sendSocket();
+    }
+
+    public void sendSocket() throws IOException {
+        Server.send(socket, data);
+    }
+
+    public void acceptSocket() {
+        connect.accept();
+    }
 }
