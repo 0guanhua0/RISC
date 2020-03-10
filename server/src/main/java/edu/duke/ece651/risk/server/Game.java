ba50790a5ecb2f151a13213c0624c18854a5fffb
ananjaya.tyagi@duke.edu <ananjaya.tyagi@duke.edu>
2020-03-09 20:57:33 -0400

    changed GameTest.java

--- server/src/main/java/edu/duke/ece651/risk/server/Game.java
+++ server/src/main/java/edu/duke/ece651/risk/server/Game.java
@@ -1,30 +1,29 @@
 package edu.duke.ece651.risk.server;
 
 import edu.duke.ece651.risk.shared.Server;
+import edu.duke.ece651.risk.shared.map.MapDataBase;
+import edu.duke.ece651.risk.shared.map.WorldMap;
 
 import java.io.IOException;
 import java.net.Socket;
 
 public class Game {
     Server connect;
-    private Socket socket;
-    private String data;
+    Socket socket;
 
     public Game() throws IOException {
-        connect = new Server();
+        connect = new Server(8080);
     }
 
     public static void main(String[] args) throws IOException {
         Game game = new Game();
         game.acceptSocket();
-        game.sendSocket();
+        Server.send(game.socket, "Hello");//new MapDataBase().getMap("a clash of kings"));
     }
 
-    public void sendSocket() throws IOException {
-        Server.send(socket, data);
-    }
+
 
     public void acceptSocket() {
-        connect.accept();
+        socket = connect.accept();
     }
 }
