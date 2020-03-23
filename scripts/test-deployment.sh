bf5890fce95963afa890cbbe170f1f69f96dbc4b
xkw168 <kewei.xia@duke.edu>
2020-03-23 19:20:06 -0400

    add testing file

--- scripts/test-deployment.sh
+++ scripts/test-deployment.sh
@@ -7,33 +7,24 @@ x=""
 count=0
 while [ "$x" == "" ]
 do
-    if [ "$count" == "30" ]
+    if [ "$count" == "20" ]
     then
         echo "Giving up after 20 attempts to connect!"
         exit 1
     fi
-    x=`netcat -N -w 1 localhost 1651 < /dev/null`
+    x=`netcat -N -w 1 localhost 12345 < /dev/null`
+    sleep 4
     let count=count+1
 done
 
+{
+    ./gradlew run-client --console=plain < ./test_files/player1.txt 
+} &
 
+sleep 3
 
-
-nc -N localhost 1651 > testoutput <<EOF
-2
-99
-123456
-EOF
-cat > expectedoutput <<EOF
-Hello, welcome to Drew's Prime Factor Server!
-Please enter a number:
-2
-Please enter a number:
-3 3 11
-Please enter a number:
-2 2 2 2 2 2 3 643
-Please enter a number:
-EOF
-
-diff testoutput expectedoutput
+{
+    ./gradlew run-client --console=plain < ./test_files/player2.txt 
+} &
+wait
 
