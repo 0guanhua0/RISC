e871d40746dff4e5ece3feb39c1f3c7ff75a56df
charlie <charlieupupup@gmail.com>
2020-03-29 10:23:02 -0400

    basic structure for validate

--- server/src/main/java/edu/duke/ece651/risk/server/SQL.java
+++ server/src/main/java/edu/duke/ece651/risk/server/SQL.java
@@ -1,4 +1,91 @@
 package edu.duke.ece651.risk.server;
 
-public class sql {
-}
+import java.sql.*;
+
+public class SQL {
+    Connection conn = null;
+
+    public SQL() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/risk", "postgres", "postgres");
+
+        } catch (Exception ignored) {
+
+        }
+    }
+
+    public boolean createTable() {
+        try {
+            Statement statement = conn.createStatement();
+            // create table
+            String sql = "CREATE TABLE risk (PLAYER_ID SERIAL PRIMARY KEY," +
+                    "NAME TEXT NOT NULL," +
+                    "PASSWORD TEXT NOT NULL);";
+            statement.executeUpdate(sql);
+            statement.close();
+            return true;
+        } catch (SQLException ignored) {
+        }
+        return false;
+    }
+
+    public boolean dropTable() {
+        try {
+            Statement statement = conn.createStatement();
+            // create table
+            String sql = "DROP TABLE IF EXISTS risk;";
+            statement.executeUpdate(sql);
+            statement.close();
+            return true;
+        } catch (SQLException ignored) {
+        }
+        return false;
+    }
+
+    public boolean addUser(String name, String pwd) {
+        // ask postgres to crypt the password
+        try {
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(String.format("INSERT INTO risk (name, password) VALUES (\n" +
+                    "  '%s',\n" +
+                    "  crypt('%s', gen_salt('bf'))\n" +
+                    ");", name, pwd));
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean authUser(String name, String pwd) {
+        try {
+            Statement statement = conn.createStatement();
+            try (ResultSet set = statement.executeQuery(String.format("SELECT PLAYER_ID \n" +
+                    "  FROM risk\n" +
+                    " WHERE name = '%s' \n" +
+                    "   AND password = crypt('%s', password);", name, pwd));) {
+                return set.next();
+            } catch (SQLException ignored) {
+            }
+        } catch (SQLException ignored) {
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        SQL sql = new SQL();
+        sql.dropTable();
+        sql.createTable();
+        if (sql.authUser("test", "123")) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+        sql.addUser("test", "123");
+        if (sql.authUser("test", "123")) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+    }
+};
--- server/src/main/java/edu/duke/ece651/risk/server/UserValidation.java
+++ server/src/main/java/edu/duke/ece651/risk/server/UserValidation.java
@@ -1,4 +1,64 @@
 package edu.duke.ece651.risk.server;
 
+import com.google.gson.Gson;
+
 public class UserValidation {
+    /**
+     * recv string and convert to json obj
+     */
+    public Boolean validate(Gson gsonObj) {
+        String userName = gsonObj.fromJson("userName", String.class);
+        String userPassword = gsonObj.fromJson("userPassword", String.class);
+        String action = gsonObj.fromJson("action", String.class);
+
+        if (action.equals("login")) {
+            return validateLogin(userName, userPassword);
+        }
+
+        if (action.equals("signup")) {
+            return validateSignup(userName, userPassword);
+        }
+
+        if (action.equals("update")) {
+            return validateUpdate(userName, userPassword);
+        }
+
+        return false;
+
+    }
+
+    /**
+     * parse json obj
+     */
+
+    /**
+     * validate login
+     */
+
+    public Boolean validateLogin(String userName, String userPassword) {
+        //check database
+
+        //
+        return false;
+    }
+
+    /**
+     * validate create
+     */
+    public Boolean validateSignup(String userName, String userPassword) {
+        //check database
+
+        //
+        return false;
+    }
+
+    /**
+     * validate update
+     */
+    public Boolean validateUpdate(String userName, String userPassword) {
+        //check database
+
+        //
+        return false;
+    }
 }
