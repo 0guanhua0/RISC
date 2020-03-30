45470c000a6808116e3e7525932ec8e0a58f3690
xkw168 <kewei.xia@duke.edu>
2020-03-30 19:07:07 -0400

    add go back dialog

--- RiscClient/app/src/main/java/edu/duke/ece651/riskclient/activity/PlayGameActivity.java
+++ RiscClient/app/src/main/java/edu/duke/ece651/riskclient/activity/PlayGameActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import edu.duke.ece651.riskclient.R;
+
+import static edu.duke.ece651.riskclient.utils.UIUtils.showToastUI;
 
 public class PlayGameActivity extends AppCompatActivity {
 
@@ -37,8 +40,23 @@ public class PlayGameActivity extends AppCompatActivity {
     }
 
     private void goBack(){
-        AlertDialog.Builder dialog = new AlertDialog.Builder(PlayGameActivity.this);
-//        dialog.se
+        AlertDialog.Builder builder = new AlertDialog.Builder(PlayGameActivity.this);
+        builder.setPositiveButton("Exit", (dialog1, which) -> {
+            showToastUI(PlayGameActivity.this, "Exit");
+        });
+        builder.setNegativeButton("Leave", (dialog2, which) -> {
+            showToastUI(PlayGameActivity.this, "Leave");
+        });
+        builder.setMessage("Do you want to exit the game or exit the game?");
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(dialog12 -> {
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                    .setTextColor(
+                            getResources().getColor(R.color.colorPrimary)
+                    );
+        });
+        dialog.show();
+        // TODO: communicate with the server, send the exit info
         onBackPressed();
     }
 }
