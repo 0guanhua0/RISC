351c8fde377982cf1a5d114ca60d8619e76094f0
charlie <charlieupupup@gmail.com>
2020-03-31 20:42:39 -0400

    framework for socket connection

--- server/src/main/java/edu/duke/ece651/risk/server/SocketMock.java
+++ server/src/main/java/edu/duke/ece651/risk/server/SocketMock.java
@@ -1,4 +1,7 @@
 package edu.duke.ece651.risk.server;
 
+/**
+ * handle the socket when user quit from the middle
+ */
 public class SocketMock {
 }
--- server/src/main/java/edu/duke/ece651/risk/server/SocketRedirect.java
+++ server/src/main/java/edu/duke/ece651/risk/server/SocketRedirect.java
@@ -1,4 +1,10 @@
 package edu.duke.ece651.risk.server;
 
+/**
+ * recognize the intention of different socket
+ * redirect socket to proper connection
+ * return according info & close socket
+ *
+ */
 public class SocketRedirect {
 }
