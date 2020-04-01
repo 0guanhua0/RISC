76e6d3b2763a55e539005bfac856e54f80dca6a6
charlie <charlieupupup@gmail.com>
2020-04-01 19:45:28 -0400

    add active room player is in

--- server/src/main/java/edu/duke/ece651/risk/server/SocketRedirect.java
+++ server/src/main/java/edu/duke/ece651/risk/server/SocketRedirect.java
@@ -1,5 +1,12 @@
 package edu.duke.ece651.risk.server;
 
+import edu.duke.ece651.risk.shared.player.Player;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import static edu.duke.ece651.risk.shared.Constant.*;
+
 /**
  * recognize the intention of different socket
  * redirect socket to proper connection
@@ -8,7 +15,18 @@ package edu.duke.ece651.risk.server;
  */
 public class SocketRedirect {
 
-    public void redirect() {
+    public void redirect(Player<?> player) throws IOException, ClassNotFoundException {
+        String msg = (String) player.recv();
+        JSONObject obj = new JSONObject(msg);
+
+        String userName = obj.getString(USER_NAME);
+        String action = obj.getString(ACTION);
+
+        //check user is in validate list
+
+        //if yes, proceed
+        //if no, return error
+
 
     }
 
--- server/src/main/java/edu/duke/ece651/risk/server/UserInfo.java
+++ server/src/main/java/edu/duke/ece651/risk/server/UserInfo.java
@@ -1,5 +1,9 @@
 package edu.duke.ece651.risk.server;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class UserInfo {
     //added room
+    //class that store the online validated player
+    private ConcurrentHashMap<String, >
 }
