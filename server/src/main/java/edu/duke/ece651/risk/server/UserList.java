632fd5066a98cedb7d9d59496be7e6a536216a99
charlie <charlieupupup@gmail.com>
2020-04-02 21:09:19 -0400

    add UserList

--- server/src/main/java/edu/duke/ece651/risk/server/UserList.java
+++ server/src/main/java/edu/duke/ece651/risk/server/UserList.java
@@ -4,17 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserList {
-    List<String> list = new ArrayList<>();
+    List<User> userList = new ArrayList<>();
 
-   public void addUser(String userName) {
-       list.add(userName);
+   public void addUser(User user) {
+       userList.add(user);
    }
 
-   public void rmUser(String userName) {
-       list.remove(userName);
+   public void rmUser(User user) {
+       userList.remove(user);
    }
+
 
    public boolean hasUser(String userName) {
-       return list.contains(userName);
+       for (User u : userList) {
+           if (u.getUserName().equals(userName)) {
+               return true;
+           }
+       }
+       return false;
+   }
+
+   public User findUser(String userName) {
+       for (User u : userList) {
+           if (u.getUserName().equals(userName)) {
+               return u;
+           }
+       }
+
+       return null;
    }
 }
