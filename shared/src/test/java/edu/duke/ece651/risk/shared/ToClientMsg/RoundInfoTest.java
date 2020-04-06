8deb6dc365032bae7d584ff9328c0abc4550ee5b
charlie <charlieupupup@gmail.com>
2020-04-06 19:31:21 -0400

    room info test

--- shared/src/test/java/edu/duke/ece651/risk/shared/ToClientMsg/RoundInfoTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/ToClientMsg/RoundInfoTest.java
@@ -1,7 +1,5 @@
 package edu.duke.ece651.risk.shared.ToClientMsg;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import edu.duke.ece651.risk.shared.map.MapDataBase;
 import edu.duke.ece651.risk.shared.map.WorldMap;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class RoundInfoTest {
 
@@ -48,6 +49,9 @@ public class RoundInfoTest {
         players.put(3, "Red");
 
         roundInfo = new RoundInfo(1, map, players, null);
+
+        //test player list
+        assertNull(roundInfo.getPlayer());
     }
 
     @Test
