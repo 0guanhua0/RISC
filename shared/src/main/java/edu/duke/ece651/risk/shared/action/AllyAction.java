b7bd4aeaf6177ec8b53b9c95b043f656338da419
BenthamWu <15924430190@163.com>
2020-04-12 11:05:54 -0400

    add a comment

--- shared/src/main/java/edu/duke/ece651/risk/shared/action/AllyAction.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/action/AllyAction.java
@@ -12,6 +12,8 @@ import java.util.List;
  * @create: 2020-04-10 17:13
  **/
 public class AllyAction implements Action{
+
+    //below the id of the player that you want ally to ally with at this round
     int allyRequest;
 
     public AllyAction(int allyRequest) {
